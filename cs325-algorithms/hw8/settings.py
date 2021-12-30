efbb6d7824556cdd051481d9f6ecf4fecc62fc70
Donald Kiser <kiserd@oregonstate.edu>
2021-12-30 09:47:47 -0800

    fixed vulnerability

--- cs325-algorithms/hw8/settings.py
+++ cs325-algorithms/hw8/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import environ
+
+# get access to environment var
+env = environ.Env()
+env.read_env(env.str('ENV_PATH', '../.env.local'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7y9+h_-xm$h_u%_%4f^n&uu4*h=4u^5=!a_*x#38+17ut1=&2+'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
